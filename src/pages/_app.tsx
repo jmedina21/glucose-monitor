87e6a92911af59a7458e28c65b2edccd7f596322
jmedina21 <jmedinamulet@gmail.com>
2025-03-01 00:46:47 -0500

    Remove default Next.js public assets and add custom favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import favicon from "./favicon.png";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>Azúcar</title>
+                <link rel="icon" href={favicon.src} />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 }
